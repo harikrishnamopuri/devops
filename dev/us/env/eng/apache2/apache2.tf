1da051c928ce910de944637e3a1d4de59ee374c9
root <root@ip-10-0-2-179.ec2.internal>
2019-12-24 10:20:24 +0000

    apache2 changes

--- dev/us/env/eng/apache2/apache2.tf
+++ dev/us/env/eng/apache2/apache2.tf
@@ -6,7 +6,21 @@ module "apache2" {
   private_subnets = data.aws_subnet.private.*.id
   ami = var.ami
   env = var.env
-  ansible_role = var.ansible_role
-  role_name  = var.role_name
-  lb_type         = var.lb_type 
+  max_size = var.max_size
+  min_size = var.min_size
+  desired_capacity = var.desired_capacity
+  ebs_type = var.ebs_type
+  ebs_size = var.ebs_size
+  ebs_delete = var.ebs_delete
+  health_check_type = var.health_check_type
+  ssh_key = aws_key_pair.deployer.key_name
+
+
+  lb_type         = var.lb_type
+  apache2_lb      = var.apache2_lb
+  apache2_ports      = var.apache2_ports
   }
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/DK2E7kV/YfhBqdQ3op0WSdLRxnJjiqQ4HV8Q2V4m0w1f5qLJFpOD0KQFGdzN6ow4YeWWNC7Yv7z51BrEzkhZxd4Fdx3zFaA/R8d4c10vJiUtw4GwoJaedmT1ENSy+NYH3gztyHgTp7a6HEC+TuZAcs9ztSBHNE/iih3sCt1omNCmLpjIj3VaOdWNl+aaXnBNQX411S9bL9VzLoAfLGSGM5Ek/UuSJAoMi1SjiOlfboMNeZaC6tJbqpx5nadoqEzRoFBSSlfY2TjnUEFz6AZo0TTrBBYDF9wdBokkv6HL+Wz5OZDRbkVy5DSk6lPMHI1bRRuLwkNbONCCjvGJadfP root@ip-10-0-2-179"
+}
--- dev/us/env/eng/apache2/variable.tf
+++ dev/us/env/eng/apache2/variable.tf
@@ -3,26 +3,20 @@ variable "region" {
   default  = "us-west-2"
 }
 
-variable "app_name" {
-  type = string
-  default  = "apache2"
-}
-
-
-# Required variables
-variable "vpc_id" {
-  type = string
-  default = "vpc-0c427ec6219254801"
-}
-
-variable "private_subnets" {
-  type = list(string)
-  default = ["subnet-02cdef7337949ff93",  "subnet-0a6c51199f52047d4",  "subnet-09b5d958f07382c7f"]
-}
-
 variable "ami" {
   type = string
   default = "ami-a2e544da"
+}
+
+variable "ssh_key" {
+  type    = string
+  default = "hari-test"
+}
+
+
+variable "app_name" {
+  type = string
+  default  = "apache2"
 }
 
 variable "env" {
@@ -30,18 +24,63 @@ variable "env" {
   default = "dev"
 }
 
-variable "ansible_role" {
-  type = string
-  default = "test"
-}
-
-variable "role_name" {
-   type = string
-   default  = "hari.mopuri"
-}
-
 variable "lb_type" {
    type = string
    default  = "nlb"
 }
+variable "max_size" {
+  type    = string
+  default = 2
+}
+
+variable "min_size" {
+  type    = string
+  default = 1
+}
+variable "desired_capacity" {
+  type    = string
+  default = 1
+}
+
+variable "ebs_type" {
+  type    = string
+  default = "gp2"
+}
+
+variable "ebs_size" {
+  type    = string
+  default = 30
+}
+
+variable "ebs_delete" {
+  type    = string
+  default = "true"
+}
+
+# Instance (autoscaling)
+
+variable "health_check_type" {
+  type    = string
+  default = "ELB"
+}
+
+variable "launch_time" {
+  type    = string
+  default = 600
+}
+
+
+variable "apache2_ports" {
+
+  default = {
+    "inbound" = "9090" 
+  }
+}
+
+variable "apache2_lb" {
+  type   = string
+  default = "true"
+
+}
+
 
